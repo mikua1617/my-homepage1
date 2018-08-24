ca23faef28a6c4af443e4591a5c9843fd08f2549
Shashwat Mishra <shashwat.mishra@zemosolabs.com>
2018-08-24 14:49:48 +0530

    added validations

--- Scripts/script.js
+++ Scripts/script.js
@@ -18,10 +18,10 @@ if(localStorage.myJSONobj === undefined){
 
 
 
-var imageJSON = [{"name": "Pic of Birthday", "imageurl": "./Images/JPEG_20180315_103249_file772084190.jpg"}, 
-           {"name": "Sinhagadh fort top", "imageurl": "./Images/IMG_20180218_103646288_HDR.jpg"},
-           {"name": "Right after getting a job", "imageurl": "./Images/JPEG_20180315_103332_file1139079576.jpg"},
-           {"name": "Jaisalmer trip", "imageurl": "./Images/JPEG_20180315_103405_file46115999.jpg"} 
+var imageJSON = [{"name": "Pic of Birthday", "imageurl": "./Images/JPEG_20180315_103249_file772084190.jpg", "date": "15/06/2017"},
+           {"name": "Sinhagadh fort top", "imageurl": "./Images/IMG_20180218_103646288_HDR.jpg", "date": "14/03/2018"},
+           {"name": "Right after getting a job", "imageurl": "./Images/JPEG_20180315_103332_file1139079576.jpg", "date": "05/09/2017"},
+           {"name": "Jaisalmer trip", "imageurl": "./Images/JPEG_20180315_103405_file46115999.jpg", "date": "12/12/2017"}
             ];
 
 }
@@ -45,6 +45,3 @@ for (var i=0; i<allimages.length; i++){
 
     addNewImage(allimages[i].name, allimages[i].imageurl);
 }
-
-
-
